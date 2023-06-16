a46061748dcad477c86fdad1c2c0b97b026511e6
karimaltohamy <karimbadr2003@gmail.com>
2023-06-16 09:41:55 +0200

    commit

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,36 @@
 const sidebar = document.querySelector(".sidebar");
 const contentPage = document.querySelector(".content_page");
 const btnMenu = document.querySelector(".icon_menu");
+const overflowSidebar = document.querySelector(".overflow_sidebar");
 
 btnMenu.addEventListener("click", () => {
   sidebar.classList.toggle("remove_elemnts");
   sidebar.classList.toggle("close_sidebar");
   contentPage.classList.toggle("close_sidebar");
+
+  if (innerWidth < 768) {
+    overflowSidebar.classList.toggle("active");
+  }
+});
+
+if (innerWidth < 768) {
+  sidebar.classList.add("remove_elemnts");
+  sidebar.classList.add("close_sidebar");
+  contentPage.classList.add("close_sidebar");
+} else {
+  sidebar.classList.remove("remove_elemnts");
+  sidebar.classList.remove("close_sidebar");
+  contentPage.classList.remove("close_sidebar");
+}
+
+window.addEventListener("resize", () => {
+  if (innerWidth < 768) {
+    sidebar.classList.add("remove_elemnts");
+    sidebar.classList.add("close_sidebar");
+    contentPage.classList.add("close_sidebar");
+  } else {
+    sidebar.classList.remove("remove_elemnts");
+    sidebar.classList.remove("close_sidebar");
+    contentPage.classList.remove("close_sidebar");
+  }
 });
